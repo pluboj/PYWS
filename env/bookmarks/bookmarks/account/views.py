9dc05c656437ab1de61fb4ab34c6164dd7844666
Pavel Lubojacky <PLubojacky@M3Global.com>
2020-08-14 14:07:29 -0700

    Add login view

--- env/bookmarks/bookmarks/account/views.py
+++ env/bookmarks/bookmarks/account/views.py
@@ -2,8 +2,13 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth import authenticate, login
 from .forms import LoginForm
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+@login_required
+def dashboard(request):
+	return render(request, 'account/dashboard.html', {'section': 'dashboard'})
+
 def user_login(request):
 	if request.method == 'POST':
 		form = LoginForm(request.POST)
